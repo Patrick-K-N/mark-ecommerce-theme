4aa064196a918abe1b801cc1826733eabd6917d2
Patrick Kamau <patricknjuguna621@gmail.com>
2023-12-20 10:08:58 -0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -71,17 +71,7 @@ const AboutPage = (props) => {
             <h3>Our Values</h3>
             <div ref={valuesRef}>
               <p>
-                Sunspel produced some of the world's earliest T-shirts. In the
-                late 1800s the business made luxury tunics and undershirts from
-                lightweight Sea Island cotton for export to the Far East and
-                other warm climates. While these garments initially had silk
-                buttoned plackets, these were removed in the early 1900s and
-                replaced with simple bound necks to reduce manufacturing costs -
-                creating the T-shirt. We've supplied the world as the T-shirt
-                has evolved from underwear to outerwear, from symbol of youthful
-                rebellion to everyday wardrobe staple, and we've spent decades
-                refining its every last aspect.
-              </p>
+               Mark’s Beauty Shop is a one-stop destination for all your beauty needs, offering a wide range of services including nail care, spa treatments, and barber equipment. We strive to cater for both individual customers and professional salons. Our commitment to quality and customer satisfaction makes us the trusted choice in the beauty industry. We give you the experience of blending luxury and professionalism at Mark’s Beauty Shop.
               <ol>
                 <li>Be an ecowear</li>
                 <li>Sophisticated and not mass-produced</li>
